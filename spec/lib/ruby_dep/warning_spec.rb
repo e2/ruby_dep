7754e024ca5f0ab1a4204b2e5230d88261640a33
Cezary Baginski <cezary@chronomantic.net>
2016-04-26 23:33:04 +0200

    fix spec descriptions

--- spec/lib/ruby_dep/warning_spec.rb
+++ spec/lib/ruby_dep/warning_spec.rb
@@ -6,40 +6,42 @@ RSpec.describe RubyDep::Warning do
     stub_const('RUBY_VERSION', ruby_version)
   end
 
-  context 'with an up-to-date Ruby' do
-    let(:ruby_version) { '2.3.1' }
-    it '#show_warnings' do
-      expect(STDERR).to_not receive(:puts)
-      subject.show_warnings
+  describe '#show_warnings' do
+    context 'with an up-to-date Ruby' do
+      let(:ruby_version) { '2.3.1' }
+      it 'does not show warning' do
+        expect(STDERR).to_not receive(:puts)
+        subject.show_warnings
+      end
     end
-  end
 
-  context 'with a secure but buggy Ruby' do
-    let(:ruby_version) { '2.2.4' }
-    it '#show_warnings' do
-      expect(STDERR).to receive(:puts).with(
-        'RubyDep: WARNING: your Ruby is outdated/buggy. Please upgrade.')
-      subject.show_warnings
+    context 'with a secure but buggy Ruby' do
+      let(:ruby_version) { '2.2.4' }
+      it 'shows warning about bugs' do
+        expect(STDERR).to receive(:puts).with(
+          'RubyDep: WARNING: your Ruby is outdated/buggy. Please upgrade.')
+        subject.show_warnings
+      end
     end
-  end
 
-  context 'with an insecure Ruby' do
-    let(:ruby_version) { '2.2.3' }
-    it '#show_warnings' do
-      expect(STDERR).to receive(:puts).with(
-        'RubyDep: WARNING: your Ruby has security vulnerabilities!'\
-        ' Please upgrade!')
-      subject.show_warnings
+    context 'with an insecure Ruby' do
+      let(:ruby_version) { '2.2.3' }
+      it 'shows warning about vulnerability' do
+        expect(STDERR).to receive(:puts).with(
+          'RubyDep: WARNING: your Ruby has security vulnerabilities!'\
+          ' Please upgrade!')
+        subject.show_warnings
+      end
     end
-  end
 
-  context 'with an unsupported Ruby' do
-    let(:ruby_version) { '1.9.3' }
-    it '#show_warnings' do
-      expect(STDERR).to receive(:puts).with(
-        'RubyDep: WARNING: your Ruby has security vulnerabilities!'\
-        ' Please upgrade!')
-      subject.show_warnings
+    context 'with an unsupported Ruby' do
+      let(:ruby_version) { '1.9.3' }
+      it 'shows warning about vulnerability' do
+        expect(STDERR).to receive(:puts).with(
+          'RubyDep: WARNING: your Ruby has security vulnerabilities!'\
+          ' Please upgrade!')
+        subject.show_warnings
+      end
     end
   end
 end
