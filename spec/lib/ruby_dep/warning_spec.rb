71cdca9ba089dbc2f863c1dac02ee9222f17c685
Cezary Baginski <cezary@chronomantic.net>
2016-05-06 03:20:46 +0200

    refactor warnings spec

--- spec/lib/ruby_dep/warning_spec.rb
+++ spec/lib/ruby_dep/warning_spec.rb
@@ -2,14 +2,16 @@ require 'ruby_dep/warning'
 
 RSpec.describe RubyDep::Warning do
   before do
-    allow(STDERR).to receive(:puts)
+    stub_const('STDERR', instance_double(IO))
     stub_const('RUBY_VERSION', ruby_version)
     stub_const('RUBY_ENGINE', ruby_engine)
+    allow(STDERR).to receive(:puts)
   end
 
   let(:ruby_engine) { 'ruby' }
 
   describe '#show_warnings' do
+    before { subject.show_warnings }
     context 'when silenced' do
       around do |example|
         old = ENV['RUBY_DEP_GEM_SILENCE_WARNINGS']
@@ -21,8 +23,7 @@ RSpec.describe RubyDep::Warning do
       context 'with any outdated Ruby' do
         let(:ruby_version) { '1.9.3' }
         it 'does not show warning' do
-          expect(STDERR).to_not receive(:puts)
-          subject.show_warnings
+          expect(STDERR).to_not have_received(:puts)
         end
       end
     end
@@ -31,35 +32,39 @@ RSpec.describe RubyDep::Warning do
       context 'with an up-to-date Ruby' do
         let(:ruby_version) { '2.3.1' }
         it 'does not show warning' do
-          expect(STDERR).to_not receive(:puts)
-          subject.show_warnings
+          expect(STDERR).to_not have_received(:puts)
         end
       end
 
       context 'with a secure but buggy Ruby' do
         let(:ruby_version) { '2.2.4' }
         it 'shows warning about bugs' do
-          expect(STDERR).to receive(:puts).with(
+          expect(STDERR).to have_received(:puts).with(
             %r{RubyDep: WARNING: your Ruby is outdated\/buggy.})
-          subject.show_warnings
         end
       end
 
       context 'with an insecure Ruby' do
         let(:ruby_version) { '2.2.3' }
         it 'shows warning about vulnerability' do
-          expect(STDERR).to receive(:puts).with(
+          expect(STDERR).to have_received(:puts).with(
             /RubyDep: WARNING: your Ruby has security vulnerabilities!/)
-          subject.show_warnings
+        end
+      end
+
+      context 'with an insecure base Ruby' do
+        let(:ruby_version) { '2.2.3' }
+        it 'shows warning about vulnerability' do
+          expect(STDERR).to have_received(:puts).with(
+            /RubyDep: WARNING: your Ruby has security vulnerabilities!/)
         end
       end
 
       context 'with an unsupported Ruby' do
         let(:ruby_version) { '1.9.3' }
         it 'shows warning about vulnerability' do
-          expect(STDERR).to receive(:puts).with(
+          expect(STDERR).to have_received(:puts).with(
             /RubyDep: WARNING: your Ruby has security vulnerabilities!/)
-          subject.show_warnings
         end
       end
 
@@ -68,8 +73,7 @@ RSpec.describe RubyDep::Warning do
           let(:ruby_version) { '2.2.3' }
           let(:ruby_engine) { 'jruby' }
           it 'does not show warning about vulnerability' do
-            expect(STDERR).to_not receive(:puts)
-            subject.show_warnings
+            expect(STDERR).to_not have_received(:puts)
           end
         end
       end
@@ -79,9 +83,8 @@ RSpec.describe RubyDep::Warning do
           let(:ruby_version) { '1.2.3' }
           let(:ruby_engine) { 'ironruby' }
           it 'shows warning about vulnerability' do
-            expect(STDERR).to receive(:puts).with(
+            expect(STDERR).to have_received(:puts).with(
               /RubyDep: WARNING: your Ruby has security vulnerabilities!/)
-            subject.show_warnings
           end
         end
       end
