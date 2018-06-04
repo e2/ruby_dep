531642cca05d32217551a739cf803667ea4c8767
Olle Jonsson <olle.jonsson@gmail.com>
2018-06-04 10:04:49 +0200

    Support JRuby 9.2.0.0

--- spec/lib/ruby_dep/travis_spec.rb
+++ spec/lib/ruby_dep/travis_spec.rb
@@ -90,6 +90,13 @@ RSpec.describe RubyDep::Travis do
           expect(subject.version_constraint).to eq(['~> 2.3', '>= 2.3.3'])
         end
       end
+
+      context 'with version 9.2.0.0' do
+        let(:yml) { YAML.dump('rvm' => %w(jruby-9.2.0.0)) }
+        it 'pessimistically locks with correct initial supported version' do
+          expect(subject.version_constraint).to eq(['~> 2.5', '>= 2.5.0'])
+        end
+      end
     end
 
     context 'with multiple versions' do
